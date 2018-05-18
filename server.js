ce02444a4c6efee6d1b277c1fc06fc62d328cac0
Izzie <izziezsullivan@gmail.com>
2018-05-18 00:24:38 -0400

    heroku updates to port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql')
 // Let’s make node/socketio listen on port 3000
-var io = require('socket.io').listen(3000)
+var io = require('socket.io').listen(process.env.PORT || 3000)
+
 // Define our db creds
 var db = mysql.createConnection({
     host: 'us-cdbr-iron-east-04.cleardb.net',
